7e5506c66fc746a917d7df96f951381303fd16c2
GeekTimePass <mukeshky@gmail.com>
2021-07-11 22:40:44 +0530

    Refactoring

--- src/main/java/com/jaeger/Jaeger/JagerConfig.java
+++ src/main/java/com/jaeger/Jaeger/JagerConfig.java
@@ -3,18 +3,78 @@ package com.jaeger.Jaeger;
 import io.jaegertracing.Configuration;
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.samplers.ConstSampler;
+import io.opentracing.util.GlobalTracer;
 import org.apache.tomcat.util.bcel.Const;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import javax.annotation.PostConstruct;
 
 public class JagerConfig {
 
-    @Bean
-    public JaegerTracer tracer(){
-        return new io.jaegertracing.Configuration("Jaeger-Client")
-                .withSampler(new Configuration.SamplerConfiguration()
-                        .withType(ConstSampler.TYPE))
-                .withReporter(new Configuration.ReporterConfiguration().withLogSpans(true))
-                .getTracer();
+    @Autowired
+    private Environment env;
 
+    @Bean
+    public JaegerTracer tracer() {
+        return io.jaegertracing.Configuration.fromEnv(env.getProperty("spring.application.name"))
+                .withSampler(
+                        io.jaegertracing.Configuration.SamplerConfiguration.fromEnv()
+                                .withType(ConstSampler.TYPE)
+                                .withParam(1))
+                .withReporter(
+                        io.jaegertracing.Configuration.ReporterConfiguration.fromEnv()
+                                .withLogSpans(true)
+                                .withFlushInterval(1000)
+                                .withMaxQueueSize(10000)
+                                .withSender(
+                                        io.jaegertracing.Configuration.SenderConfiguration.fromEnv()
+                                                .withAgentHost(env.getProperty("opentracing.jaeger.udp-sender.host"))
+                                                .withAgentPort(Integer.valueOf(env.getProperty("opentracing.jaeger.udp-sender.port")))
+                                ))
+                .getTracer();
     }
+
+    /**
+     * The @PostConstruct above registers the Tracer as the GlobalTracer after initialization.
+     * This is important because many of the tracing interceptors make use of the GlobalTracer behind the scenes whenever an instance
+     * of a Tracer is not explicitly provided.
+     */
+    @PostConstruct
+    public void registerToGlobalTracer() {
+        if (!GlobalTracer.isRegistered()) {
+            GlobalTracer.register(tracer());
+        }
+    }
+
+   /* private void injectFirstKafkaConsumerSpan(String number, ProcessorContext context) {
+        Tracer tracer = GlobalTracer.get();
+        Tracer.SpanBuilder spanBuilder = tracer.buildSpan("receive")
+                .withTag(Tags.SPAN_KIND.getKey(), Tags.SPAN_KIND_CONSUMER);
+
+        Span span = spanBuilder.start();
+        Tags.COMPONENT.set(span, TRACING_COMPONENT_NAME);
+        Tags.PEER_SERVICE.set(span, TRACING_SERVICE_NAME);
+        span.setTag("partition", context.partition());
+        span.setTag("topic", context.topic());
+        span.setTag("offset", context.offset());
+        *//* add the number (aka the first consumer record value) as a span tag *//*
+        span.setTag(TRACING_NUMBER_VALUE, number);
+        span.finish();
+
+        TextMap headersMapInjectAdapter = new TextMap() {
+            @Override
+            public Iterator<Map.Entry> iterator() {
+                throw new UnsupportedOperationException("iterator should never be used with Tracer.inject()");
+            }
+
+            @Override
+            public void put(String key, String value) {
+                context.headers().add(key, value.getBytes(StandardCharsets.UTF_8));
+            }
+        };
+        tracer.inject(span.context(), Format.Builtin.TEXT_MAP, headersMapInjectAdapter);
+    }*/
+
 }
--- src/main/java/com/jaeger/JaegerDemoApplication.java
+++ src/main/java/com/jaeger/JaegerDemoApplication.java
@@ -1,11 +1,14 @@
 package com.jaeger;
 
+import io.opentracing.contrib.spring.web.client.TracingRestTemplateInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -18,6 +21,8 @@ public class JaegerDemoApplication {
 
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-		return restTemplateBuilder.build();
+		RestTemplate restTemplate = restTemplateBuilder.build();
+//		restTemplate.setInterceptors(Collections.singletonList(new TracingRestTemplateInterceptor()));
+		return restTemplate;
 	}
 }
