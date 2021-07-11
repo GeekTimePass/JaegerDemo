7d4510205060e758a4c9249318a7476d1eba99f7
GeekTimePass <mukeshky@gmail.com>
2021-07-11 22:07:33 +0530

    Refactoring

--- src/main/java/com/jaeger/controller/SpringKafkaController.java
+++ src/main/java/com/jaeger/controller/SpringKafkaController.java
@@ -1,6 +1,9 @@
 package com.jaeger.controller;
 
+import com.google.common.collect.ImmutableMap;
 import com.jaeger.spring.kafka.KafkaProducer;
+import io.opentracing.Span;
+import io.opentracing.contrib.tracerresolver.TracerResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,8 +16,17 @@ public class SpringKafkaController {
 
 	@GetMapping(value = "/producer/{message}")
 	public String producer(@PathVariable("message") String message) throws Exception{
-		kafkaSender.send("test1", message);
-		return "Message sent to the Kafka Topic java_in_use_topic Successfully";
+		Span span = TracerResolver.resolveTracer().activeSpan();
+		try {
+			span.setBaggageItem("baggage", "Mukesh Baggage");
+			span.log("Preparing to send message to kafka..");
+			kafkaSender.send("test1", message);
+			span.log(ImmutableMap.of("event", "Kafka Sender", "message", message));
+			span.log("Message Sent");
+			return "Message sent to the Kafka Topic java_in_use_topic Successfully";
+		} finally {
+			span.finish();
+		}
 	}
 
 }
